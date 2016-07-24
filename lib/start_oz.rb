f9cd3dc463c62f1fbb2a5230bb61a86152511dd7
Matthew Swan <swanm86@gmail.com>
2016-07-24 17:24:01 -0500

    make more rubyish

--- lib/start_oz.rb
+++ lib/start_oz.rb
@@ -2,7 +2,7 @@ def begin_oz(string)
   return 'oz' if string[0, 2] == 'oz'
   return 'o'  if string[0]    == 'o' && string[1] != 'z'
   return 'z'  if string[0]    != 'o' && string[1] == 'z'
-  return string
+  string
   # if string[0, 2] == "oz"
     # string[0, 2]
   # elsif string[0] == "o" && string[1] != "z"
